e2af3486c7894a61e301b313d191ac9a797cf3b7
SangilYun <nasuler@gmail.com>
2019-02-16 16:49:57 +0900

    API server for smart brain

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('This is working')
+})
+
+app.listen(3000, ()=>{
+    console.log("app is running on port 3000");
+})
+
+
+
+
+/*
+    / --> res = this is working
+    /signin --> POST = success/fail
+    /register --> POST = user
+    /profile/:userId --> GET = user
+    
+*/
