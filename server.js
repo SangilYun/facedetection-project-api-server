cb6fe7499e18d3e9f8011700f74b04aff3a0cef5
SangilYun <nasuler@gmail.com>
2019-02-16 21:38:24 +0900

    add registering users

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/signin', (req, res) =>{
     });
     if(req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password){
-            res.json('success');
+            res.json(database.users[0]);
         }
     else{
         res.status(400).json('error logging in');
@@ -59,7 +59,6 @@ app.post('/register', (req, res) =>{
         id :'125',
         name: name,
         email: email,
-        password: password,
         entries : 0,
         joined : new Date(),
     });
