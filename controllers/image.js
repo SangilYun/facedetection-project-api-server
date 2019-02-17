ecdbfe8605288fe4be02a2298113b7853440d141
SangilYun <nasuler@gmail.com>
2019-02-17 19:53:12 +0900

    move api key from frontend to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai=require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'dd67f765be9643ca8b3fbd6962508e7c'
+   });
+   
+   const handleApiCall = (req, res) =>{
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data =>{
+            res.json(data)
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+   }
+
 const handleImage = (req, res, db) =>{
     const {id} = req.body;
     
@@ -10,4 +25,4 @@ const handleImage = (req, res, db) =>{
     .catch(err=>res.status(400).json('unable to get entries'))
 }
 
-module.exports = {handleImage}
+module.exports = {handleImage, handleApiCall}
