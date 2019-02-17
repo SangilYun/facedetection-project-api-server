a4c043db2fe69afdc0086b7e678f82872923cae2
SangilYun <nasuler@gmail.com>
2019-02-17 16:29:42 +0900

    increase entries whenever user submit

--- server.js
+++ server.js
@@ -117,19 +117,13 @@ app.get('/profile/:id', (req, res) =>{
 
 app.put('/image', (req, res) =>{
     const {id} = req.body;
-    let found = false;
-    
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found=true;
-            user.entries ++;
-            return res.json(user.entries);
-        }
-    });
-    
-    if(!found){        
-        res.status(404).json('no such user');
-    }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries=>{
+        res.json({'entries':  entries[0]});
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 });
 
 app.listen(3000, ()=>{
