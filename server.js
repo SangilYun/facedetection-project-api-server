c339223f27974ccd48bbb0e60571d95e137ad58c
SangilYun <nasuler@gmail.com>
2019-02-17 17:10:10 +0900

    add signin feature. when user try to signin, if the saved hash of password matches the password user has sent the system will allow user login otherwise give an error

--- server.js
+++ server.js
@@ -69,35 +69,50 @@ app.get('/', (req, res) =>{
 })
 
 app.post('/signin', (req, res) =>{
-    bcrypt.compare("apples", '$2a$10$LLwKGuBR94BhdzxnD/r59.b1VnpO.9qjKaawVOCUuGudQqTafCNWO', function(err, res) {
-        console.log('first guess', res);
-    });
-
-    bcrypt.compare("veggies", '$2a$10$LLwKGuBR94BhdzxnD/r59.b1VnpO.9qjKaawVOCUuGudQqTafCNWO', function(err, res) {
-        console.log('second guess', res);
-    });
-    if(req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password){
-            res.json(database.users[0]);
-        }
-    else{
-        res.status(400).json('error logging in');
-    }
-
+    db.select('email', 'hash').from('login')
+        .where('email','=', req.body.email)
+        .then(data =>{
+            const isValid=bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid){
+                return db.select('*').from('users').where('email', '=', req.body.email)
+                .then(user=>{
+                    console.log('user : '+ user);
+                    res.json(user[0])
+                })
+                .catch(err=>res.status(400).json('unable to get user'))
+            }else{
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err=>res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) =>{
     const {email, name, password } = req.body;
-
-    db('users')
-    .returning('*')
-    .insert({
-        email : email,
-        name: name,
-        joined : new Date()
-    }).then(user =>{
-        res.json(user[0]);
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx =>{
+        trx.insert({
+            hash: hash,
+            email : email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail =>{
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email : loginEmail[0],
+                    name: name,
+                    joined : new Date()
+                })
+                .then(user =>{
+                    res.json(user[0]);
+                })
+         })
+         .then(trx.commit)
+         .catch(trx.rollback);
     })
+    
     .catch(err => res.status(400).json('unable to register'))
 });
 
