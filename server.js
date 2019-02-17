a70304fd1bc16bc53ecedf69ae6666911cdbe915
SangilYun <nasuler@gmail.com>
2019-02-17 15:45:07 +0900

    view profile feature

--- server.js
+++ server.js
@@ -89,28 +89,30 @@ app.post('/signin', (req, res) =>{
 app.post('/register', (req, res) =>{
     const {email, name, password } = req.body;
 
-    db('users').insert({
+    db('users')
+    .returning('*')
+    .insert({
         email : email,
         name: name,
         joined : new Date()
-    }).then(console.log)
-    res.json(database.users[database.users.length-1]);
+    }).then(user =>{
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register'))
 });
 
 app.get('/profile/:id', (req, res) =>{
     const {id} = req.params;
-    let found = false;
     
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found=true;
-            return res.json(user);
+    db.select('*').from('users').where({id})
+    .then(user=>{
+        if(user.length){
+            res.json(user[0]);
+        }else{
+            res.status(400).json('Not Found')
         }
-    });
-
-    if(!found){        
-        res.status(404).json('no such user');
-    }
+    })
+    .catch(err=>res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) =>{
